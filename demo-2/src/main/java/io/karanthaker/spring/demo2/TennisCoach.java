fea9f0167f92c26ae27185795b4338917a6022fd
p-karanthaker <p-karanthaker@users.noreply.github.com>
2019-05-14 22:44:45 +0100

    Using Default Annotation Value
    Removed explicit naming of annotation to make use of default camel cased
    annotation.

--- demo-2/src/main/java/io/karanthaker/spring/demo2/TennisCoach.java
+++ demo-2/src/main/java/io/karanthaker/spring/demo2/TennisCoach.java
@@ -2,7 +2,7 @@ package io.karanthaker.spring.demo2;
 
 import org.springframework.stereotype.Component;
 
-@Component("tennisCoach")
+@Component
 public class TennisCoach implements Coach {
 
   @Override
