d65fba0bf422eb8bf9a1f0f03b403916102122c9
p-karanthaker <p-karanthaker@users.noreply.github.com>
2019-05-18 12:28:10 +0100

    Practice Activity #5
    DI with annotations. Reading from file.

--- demo-2/src/main/java/io/karanthaker/spring/demo2/RandomFortuneService.java
+++ demo-2/src/main/java/io/karanthaker/spring/demo2/RandomFortuneService.java
@@ -1,21 +1,36 @@
 package io.karanthaker.spring.demo2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 
 @Component
 public class RandomFortuneService implements FortuneService {
 
-  private final String[] fortunes = {
-      "Good fortune.",
-      "Bad fortune.",
-      "Who knows?"
-  };
+  private final List<String> fortunes;
+
+  public RandomFortuneService() throws FileNotFoundException {
+    fortunes = new LinkedList<>();
+    File theFile = ResourceUtils.getFile("classpath:fortunes.txt");
+    Scanner scanner = new Scanner(theFile);
+
+    while (scanner.hasNext()) {
+      fortunes.add(scanner.nextLine());
+    }
+  }
 
   @Override
   public String getFortune() {
-    int random = new Random().nextInt(fortunes.length);
-    return fortunes[random];
+    int random = new Random().nextInt(fortunes.size());
+    return fortunes.get(random);
   }
 
 }
